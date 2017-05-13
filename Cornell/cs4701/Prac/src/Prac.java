72de59aca10f180f5b52e2cb404663b6b876738e
jwp258 <jwp258@cornell.edu>
2017-05-13 14:23:32 -0400

    beginnings of quickstart

--- Cornell/cs4701/Prac/src/Prac.java
+++ Cornell/cs4701/Prac/src/Prac.java
@@ -14,13 +14,13 @@ import gameInterface.AIInterface;
 
 
 public class Prac implements AIInterface {
-	
+
 	private Key inputKey;
 	private boolean player;
 	private FrameData frameData;
 	private LinkedList<Move> actions = new LinkedList<Move>();
 
-	
+
 	@Override
 	public void close() {
 		// TODO Auto-generated method stub
@@ -36,12 +36,10 @@ public class Prac implements AIInterface {
 	public void getInformation(FrameData frameData) {
 		// TODO Auto-generated method stub
 		this.frameData = frameData;
-		try{
-		State s = new State(frameData,player);
-		Move m = check_results(s);
-		System.out.println(m.get_result());
-		}catch(Exception e){
-			System.out.println("Could not fetch framedata");
+		if(!frameData.getEmptyFlag()){
+			State s = new State(frameData,player);
+			Move m = check_results(s);
+			if(m!=null) System.out.println(m.get_result());
 		}
 	}
 
@@ -57,12 +55,12 @@ public class Prac implements AIInterface {
 	@Override
 	public Key input() {
 		// TODO Auto-generated method stub
-		try{
-		State state = new State(frameData,player);
-		Move currMove = new Move(inputKey,state);
-		actions.add(currMove);
-		}catch(Exception e){
-			System.out.println("could not fetch frameData");
+		if(!frameData.getEmptyFlag()){
+			State state = new State(frameData,player);
+			Move currMove = new Move(inputKey,state);
+			if(!is_empty_key(inputKey)){
+				actions.add(currMove);
+			}
 		}
 		return inputKey;
 	}
@@ -70,14 +68,15 @@ public class Prac implements AIInterface {
 	@Override
 	public void processing() {
 		// TODO Auto-generated method stub
-		//create the environment vector
-		
-		if(!frameData.getEmptyFlag()){
+		inputKey = rand_key();
+		/*if(!frameData.getEmptyFlag()){
 			inputKey = frameData.getKeyData().getOpponentKey(player);
-			inputKey.R = inputKey.R ? false : true;
-			inputKey.L = inputKey.L ? false : true;
-		}
+			//inputKey.R = inputKey.R ? false : true;
+			//inputKey.L = inputKey.L ? false : true;
+		}*/
 	}
+
+	//UTILITY FUNCTIONS
 	
 	public Move check_results(State s){
 		Move m = actions.peek();
@@ -94,5 +93,41 @@ public class Prac implements AIInterface {
 		return m;
 	}
 	
+	public boolean is_empty_key(Key k){
+		if(k==null) return true;
+		if(k.A || k.B || k.C || k.D || k.L || k.R || k.U) {
+			//System.out.println("non empty key");
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean is_same_key(Key k, Key k2){
+		if(k==null||k2==null){
+			return true;
+		}
+		if(k.A == k2.A && k.B == k2.B && k.C == k2.C
+				&& k.D == k2.D && k.L == k2.L && k.R == k2.R && k.U == k2.U ){
+			return true;
+		}
+		return false;
+	}
+	
+	public Key rand_key(){
+		Key r = new Key();
+		int i = (int) (Math.random()*256);
+		String s = Integer.toBinaryString(i);
+		String f = ("00000000" + s).substring(s.length());
+		System.out.println(f);
+		r.A = (Character.getNumericValue(f.charAt(1)) > 0);
+		r.B = (Character.getNumericValue(f.charAt(2)) > 0);
+		r.C = (Character.getNumericValue(f.charAt(3)) > 0);
+		r.D = (Character.getNumericValue(f.charAt(4)) > 0);
+		r.L = (Character.getNumericValue(f.charAt(5)) > 0);
+		r.R = (Character.getNumericValue(f.charAt(6)) > 0);
+		r.U = (Character.getNumericValue(f.charAt(7)) > 0);
+		return r;
+	}
+
 
 }
---
+++ Cornell/cs4701/Prac/src/QuickStart.java
@@ -0,0 +1,15 @@
+import structs.Key;
+
+public class QuickStart {
+	/*
+	 * The purpose of this class is to give the ai a semi-intelligent hard coded basis
+	 * so that it doesn't flounder early on before the KNN is populated with data.
+	 */
+	
+	public Key decision(State s){
+		
+		
+		return null;
+	}
+
+}
