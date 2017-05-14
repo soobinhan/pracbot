ff6a680ae7e3e0f1e6fbbca553cc2686a2a542ae
soobinhan <sh767@cornell.edu>
2017-05-13 22:50:41 -0400

    actually knn

--- Cornell/cs4701/Prac/src/kNN.java
+++ Cornell/cs4701/Prac/src/kNN.java
@@ -2,16 +2,21 @@ import java.util.Deque;
 import java.util.Iterator;
 import java.util.LinkedList;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
+
 import enumerate.Action;
 import structs.FrameData;
 
 public class kNN {
-	Deque<Tuple> data;
+	LinkedList<Tuple> data;
 	int size;
 	final int sim_threshold = 100;
 	final int size_limit = 3000;
 	boolean player;
 	int toggled;
+	int k = 5;
 
 	public kNN(boolean player){
 		this.data = new LinkedList<Tuple>();
@@ -35,7 +40,22 @@ public class kNN {
 											 f1.getMyCharacter(player).bottom);
 		return -Math.sqrt(Math.pow(relX_f2-relX_f1, 2) + Math.pow(relY_f2 - relY_f1, 2));
 	}
-
+	
+	static int[] argsort(double[] orig, int nummax) {
+        double[] copy = Arrays.copyOf(orig,orig.length);
+        Arrays.sort(copy);
+        double[] honey = Arrays.copyOfRange(copy,copy.length - nummax, copy.length);
+        int[] result = new int[nummax];
+        int resultPos = 0;
+        for(int i = 0; i < orig.length; i++) {
+            double onTrial = orig[i];
+            int index = Arrays.binarySearch(honey,onTrial);
+            if(index < 0) continue;
+            result[resultPos++] = i;
+        }
+        return result;
+	}
+	
 	public Deque<Action> getNearest(FrameData s){
 		
 		if(toggled == 0){
@@ -51,13 +71,36 @@ public class kNN {
 		for(int i = 0; i < this.size; i++){
 			cur_tuple = it.next();
 			scores[i] = computeSim(s, cur_tuple.fd);
-
-			if (max == null || scores[i] > max){
-				max = scores[i];
-				max_moves = cur_tuple.opp_act;
-			}
+//			if (max == null || scores[i] > max){
+//				max = scores[i];
+//				max_moves = cur_tuple.opp_act;
+//			}
 		}
-		return max_moves;
+		
+		int[] argsorted = argsort(scores, k);
+		double[] sorted = new double[k];
+		double sum = 0;
+		for (int i = 0; i < k; i++){
+			sorted[i] = scores[argsorted[i]];
+			sum += scores[argsorted[i]];
+		}
+		for(int i = 0; i < k; i++){
+			sorted[i] /= sum;
+		}
+		double[] prob = new double[k];
+		double sum_so_far = 0;
+		for(int i = 0; i < k; i++){
+			prob[i] = sum_so_far + prob[i];
+			sum_so_far += prob[i];
+		}
+		
+		double rn = Math.random();
+		int idx;
+		for(idx = 0; idx < k; idx++){
+			if(rn>=prob[idx]){break;}
+		}
+		
+		return data.get(idx).opp_act;
 	}
 	
 	public void print_actions(Deque<Action> acts){
